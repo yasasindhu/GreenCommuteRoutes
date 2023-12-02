d7c2fa7b4e0d77c6707a518ab9e2065722183dbd
sindhu <yasasindhu32@gmail.com>
2023-12-02 16:37:18 +0530

    update api

--- api/index.js
+++ api/index.js
@@ -8,10 +8,8 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    // '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
 server.listen(3000, () => {
