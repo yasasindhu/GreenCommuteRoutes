3613953a91fc5df075454d6ccf1489bd34962803
sindhu <yasasindhu32@gmail.com>
2023-12-02 16:04:30 +0530

    update vercel json file

--- api/index.js
+++ api/index.js
@@ -2,9 +2,10 @@ const jsonServer=require("json-server");
 const server=jsonServer.create();
 const router=jsonServer.router("./data/db.json");
 const middlewares=jsonServer.defaults();
-const port="https://sindhu-green-commute-routes.vercel.app/api";
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(8000);
+
+module.exports = server
