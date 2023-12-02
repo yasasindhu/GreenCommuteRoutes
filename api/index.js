05a665c1556d6ec7c0a41ad276467ffc46a61df5
sindhu <yasasindhu32@gmail.com>
2023-12-02 15:43:50 +0530

    update env

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const jsonServer=require("json-server");
 const server=jsonServer.create();
 const router=jsonServer.router("./data/db.json");
 const middlewares=jsonServer.defaults();
-const port=process.env.PORT || 8000;
+const port="https://sindhu-green-commute-routes.vercel.app/api";
 
 server.use(middlewares);
 server.use(router);
